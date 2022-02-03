582e9fc2506688ad99e8ae7b9a94c72c8233245c
ManoelNohra <98428410+ManoelNohra@users.noreply.github.com>
2022-02-03 16:17:23 -0500

    Add files via upload

---
+++ App/Application.js
@@ -0,0 +1,65 @@
+﻿class Application {
+  constructor(window, vueListeAnime, vueAnime, vueAjouterAnime, AnimeDAO){
+
+    this.window = window;
+
+    this.vueListeAnime = vueListeAnime;
+
+    this.vueAnime = vueAnime;
+
+    this.vueAjouterAnime = vueAjouterAnime;
+
+    this.vueAjouterAnime.initialiserAjouterAnime(anime =>this.ajouterAnime(anime));
+
+    this.AnimeDAO = AnimeDAO;
+
+    // C'est l'équivalent de function(){this.naviguer()}
+    this.window.addEventListener("hashchange", () =>this.naviguer());
+
+    this.naviguer();
+  }
+
+  naviguer(){
+    let hash = window.location.hash;
+
+    if(!hash){
+
+      this.AnimeDAO.lister((listeAnime) => this.afficherNouvelleListeAnime(listeAnime));
+
+    }else if(hash.match(/^#ajouter-Anime/)){
+
+      this.vueAjouterAnime.afficher();
+
+    }else{
+
+      let navigation = hash.match(/^#Anime\/([0-9]+)/);
+      let idAnime = navigation[1];
+
+      this.AnimeDAO.chercher(idAnime, (anime) => this.afficherNouveauAnime(anime));
+    }
+  }
+
+  afficherNouvelleListeAnime(listeAnime){
+
+    console.log(listeAnime);
+    this.vueListeAnime.initialiserListeAnime(listeAnime);
+    this.vueListeAnime.afficher();
+  }
+
+  afficherNouveauAnime(anime){
+    console.log(anime);
+    this.vueAnime.initialiserAnime(anime);
+    this.vueAnime.afficher();
+  }
+
+  ajouterAnime(anime){
+    this.AnimeDAO.ajouter(anime, () => this.afficherListeAnime());
+  }
+
+  afficherListeAnime(){
+    this.window.location.hash = "#";
+  }
+}
+
+new Application(window, new VueListeAnime(), new VueAnime(), new VueAjouterAnime(), new AnimeDAO());
+
