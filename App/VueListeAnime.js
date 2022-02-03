a2e839392a83e4cce9c760e6b4376f1378777450
ManoelNohra <98428410+ManoelNohra@users.noreply.github.com>
2022-02-03 16:17:41 -0500

    Add files via upload

---
+++ App/VueListeAnime.js
@@ -0,0 +1,28 @@
+﻿class VueListeAnime{
+  constructor(){
+    this.html = document.getElementById("html-vue-liste-cadeau").innerHTML;
+    this.listeCadeauDonnee = null;
+  }
+
+  initialiserListeCadeau(listeCadeauDonnee){
+    this.listeCadeauDonnee = listeCadeauDonnee;
+  }
+
+  afficher(){
+    document.getElementsByTagName("body")[0].innerHTML = this.html;
+
+    let listeCadeau = document.getElementById("liste-cadeau");
+    const listeCadeauItemHTML = listeCadeau.innerHTML;
+    let listeCadeauHTMLRemplacement = "";
+
+    for(var numeroCadeau in this.listeCadeauDonnee){
+      let listeCadeauItemHTMLRemplacement = listeCadeauItemHTML;
+      listeCadeauItemHTMLRemplacement = listeCadeauItemHTMLRemplacement.replace("{Cadeau.id}",this.listeCadeauDonnee[numeroCadeau].id);
+      listeCadeauItemHTMLRemplacement = listeCadeauItemHTMLRemplacement.replace("{Cadeau.nom}",this.listeCadeauDonnee[numeroCadeau].nom);
+      listeCadeauHTMLRemplacement += listeCadeauItemHTMLRemplacement;
+    }
+
+    listeCadeau.innerHTML = listeCadeauHTMLRemplacement;
+  }
+
+}
